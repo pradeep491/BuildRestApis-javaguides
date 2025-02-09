997f6e2b50451df598f49cdf23abe7f118167bf8
pradeep491 <16057588+pradeep491@users.noreply.github.com>
2025-02-09 23:30:56 +0530

    pagination_application

---
+++ springboot-blog-rest-api-pagination/src/main/java/com/test/SpringbootBlogRestApiPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogRestApiPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestApiPaginationApplication.class, args);
+	}
+
+}
---
+++ springboot-blog-rest-api-pagination/src/main/java/com/test/blog/service/PostService.java
@@ -0,0 +1,15 @@
+package com.test.blog.service;
+
+import com.test.blog.dtos.PostDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface PostService {
+    PostDTO createpost(PostDTO dto);
+    List<PostDTO> getAllPosts(int pageNo,int pageSize);
+    PostDTO getPostById(Long id);
+    PostDTO updatePost(Long id,PostDTO dto);
+    String deletePost(Long id);
+}
