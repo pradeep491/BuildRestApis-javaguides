86e4ad0565c7b9ef28bc698e443a2a1674eeb75a
pradeep491 <16057588+pradeep491@users.noreply.github.com>
2025-02-15 23:49:02 +0530

    sorting

---
+++ springboot-blog-rest-api-pagination-sorting-ASC-DESC/src/main/java/com/test/blog/controller/PostController.java
@@ -0,0 +1,54 @@
+package com.test.blog.controller;
+
+import com.test.blog.dtos.PostDTO;
+import com.test.blog.dtos.PostResponse;
+import com.test.blog.entity.Post;
+import com.test.blog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private PostService service;
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    //http://localhost:8088/api/posts
+    //create blog post Rest API
+    @PostMapping
+    public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO dto) {
+        return new ResponseEntity<>(service.createpost(dto), HttpStatus.CREATED);
+    }
+
+    //http://localhost:8088/api/posts?pageNo=1&pageSize=5
+    @GetMapping
+    public PostResponse getAllPosts(
+            @RequestParam(value="pageNo",defaultValue = "0",required = false) int pageNo,
+            @RequestParam(value="pageSize",defaultValue = "10",required = false) int pageSize,
+            @RequestParam(value="sortBy",defaultValue = "id",required = false) String sortBy){
+        return service.getAllPosts(pageNo,pageSize,sortBy);
+    }
+
+    //http://localhost:8088/api/posts/1
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> getPostById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getPostById(id));
+    }
+    //http://localhost:8088/api/posts/7
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDTO> updatePost(@PathVariable("id") Long id,@RequestBody PostDTO post){
+        return ResponseEntity.ok(service.updatePost(id,post));
+    }
+    //http://localhost:8088/api/posts/7
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable("id") Long id){
+        return ResponseEntity.ok(service.deletePost(id));
+    }
+}
---
+++ springboot-blog-rest-api-pagination-sorting-ASC-DESC/src/main/java/com/test/blog/service/PostService.java
@@ -0,0 +1,16 @@
+package com.test.blog.service;
+
+import com.test.blog.dtos.PostDTO;
+import com.test.blog.dtos.PostResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface PostService {
+    PostDTO createpost(PostDTO dto);
+    PostResponse getAllPosts(int pageNo, int pageSize,String sortBy);
+    PostDTO getPostById(Long id);
+    PostDTO updatePost(Long id,PostDTO dto);
+    String deletePost(Long id);
+}
