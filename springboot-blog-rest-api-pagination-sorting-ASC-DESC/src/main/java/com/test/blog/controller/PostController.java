cf0899a830e19624d051c28d62122abe3438a787
pradeep491 <16057588+pradeep491@users.noreply.github.com>
2025-02-16 00:23:13 +0530

    code_refactoring

--- springboot-blog-rest-api-pagination-sorting-ASC-DESC/src/main/java/com/test/blog/controller/PostController.java
+++ springboot-blog-rest-api-pagination-sorting-ASC-DESC/src/main/java/com/test/blog/controller/PostController.java
@@ -4,6 +4,7 @@ import com.test.blog.dtos.PostDTO;
 import com.test.blog.dtos.PostResponse;
 import com.test.blog.entity.Post;
 import com.test.blog.service.PostService;
+import com.test.blog.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,10 +35,10 @@ public class PostController {
     //http://localhost:8088/api/posts?pageNo=0&pageSize=4&sortBy=id&sortDir=desc
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir) {
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
         return service.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
