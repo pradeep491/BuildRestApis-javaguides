60d4f14d3a4b34e3ae6545a97a8659e2d4307104
pradeep491 <16057588+pradeep491@users.noreply.github.com>
2025-02-15 22:54:06 +0530

    custom_pagination

---
+++ springboot-blog-rest-api-pagination-customResponse/src/main/java/com/test/SpringbootBlogRestApiPaginationCustomResponseApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogRestApiPaginationCustomResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestApiPaginationCustomResponseApplication.class, args);
+	}
+
+}
