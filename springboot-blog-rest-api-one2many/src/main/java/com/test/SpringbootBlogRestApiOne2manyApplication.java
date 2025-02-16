291f445e956f63d99a0d9e59c7d272aa5c4aa704
pradeep491 <16057588+pradeep491@users.noreply.github.com>
2025-02-16 18:10:41 +0530

    one2many_app_changes

---
+++ springboot-blog-rest-api-one2many/src/main/java/com/test/SpringbootBlogRestApiOne2manyApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogRestApiOne2manyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestApiOne2manyApplication.class, args);
+	}
+
+}
