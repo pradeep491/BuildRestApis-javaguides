331e9e5f8605f01bbd78d8da679fa939f239a0d1
pradeep_punnu <k.pradeep0491@gmail.com>
2025-09-07 09:56:59 +0530

    code_changes

---
+++ springboot-blog-rest-api-springsecurity-section19/src/main/java/com/test/blog/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.test.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) ->
+                        //authorize.anyRequest().authenticated())
+                        authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    //commented the below InMemory Object as we are going with DB Authentication
+    /*@Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails pradeep1 = User.builder().
+                username("pradeep1")
+                .password(passwordEncoder().encode("pradeep@491"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder().
+                username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(pradeep1,admin);
+    }*/
+}
---
+++ springboot-blog-rest-api-springsecurity-section19/src/main/java/com/test/blog/service/impl/CommentServiceImpl.java
@@ -0,0 +1,120 @@
+package com.test.blog.service.impl;
+
+import com.test.blog.dtos.CommentDTO;
+import com.test.blog.entity.Comment;
+import com.test.blog.entity.Post;
+import com.test.blog.exception.BlogAPIException;
+import com.test.blog.exception.ResourceNotFoundException;
+import com.test.blog.repository.CommentRepository;
+import com.test.blog.repository.PostRepository;
+import com.test.blog.service.CommentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private CommentRepository commentRepository;
+    private PostRepository postRepository;
+    private final ModelMapper modelMapper;
+
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository,
+                              ModelMapper modelMapper) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public CommentDTO createComment(Long postId, CommentDTO dto) {
+        Comment comment = mapToEntity(dto);
+
+        //Retrieve POST Entity by id
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("post", "id", postId));
+        //set post to comment entity
+        comment.setPost(post);
+
+        //save comment entity to the DB
+        Comment savedComment = commentRepository.save(comment);
+        return mapToDTO(savedComment);
+    }
+
+    @Override
+    public List<CommentDTO> getCommentsByPostId(Long postId) {
+        //retrieve comments by post id
+        List<Comment> comments = commentRepository.findByPostId(postId);
+        //convert list of comment entities to a list of comment DTO's
+        return comments.stream().map(comment -> mapToDTO(comment)).toList();
+    }
+
+    @Override
+    public CommentDTO getCommentById(Long postId, Long commentId) {
+        //Retrieve POST Entity by id
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("post", "id", postId));
+
+        //Retrieve comment by id
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("comment", "commentId", commentId));
+        if (!comment.getPost().getId().equals(post.getId())) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "comment does not belong to post");
+        }
+        return mapToDTO(comment);
+    }
+
+    @Override
+    public CommentDTO updateCommentById(Long postId, Long commentId, CommentDTO dto) {
+        //Retrieve POST Entity by id
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("post", "id", postId));
+        //Retrieve comment by id
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("comment", "commentId", commentId));
+
+        if (!comment.getPost().getId().equals(post.getId())) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "comment does not belong to post");
+        }
+        comment.setName(dto.getName());
+        comment.setEmail(dto.getEmail());
+        comment.setBody(dto.getBody());
+
+        Comment updatedEntity = commentRepository.save(comment);
+        return mapToDTO(updatedEntity);
+    }
+
+    @Override
+    public void deleteComment(Long postId, Long commentId) {
+        //Retrieve POST Entity by id
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("post", "id", postId));
+        //Retrieve comment by id
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("comment", "commentId", commentId));
+
+        if (!comment.getPost().getId().equals(post.getId())) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "comment does not belong to post");
+        }
+
+        commentRepository.delete(comment);
+    }
+
+
+    private CommentDTO mapToDTO(Comment comment) {
+       /* CommentDTO dto = new CommentDTO();
+        dto.setId(comment.getId());
+        dto.setName(comment.getName());
+        dto.setEmail(comment.getEmail());
+        dto.setBody(comment.getBody());
+        return dto;*/
+        CommentDTO dto = modelMapper.map(comment, CommentDTO.class);
+        return dto;
+    }
+
+    private Comment mapToEntity(CommentDTO commentDto) {
+        /*Comment comment = new Comment();
+        comment.setId(commentDto.getId());
+        comment.setName(commentDto.getName());
+        comment.setEmail(commentDto.getEmail());
+        comment.setBody(commentDto.getBody());
+        return comment;*/
+        Comment comment = modelMapper.map(commentDto, Comment.class);
+        return comment;
+    }
+}
