19cd8601d499ec847d9c5887bb1c17212626b595
pradeep_punnu <k.pradeep0491@gmail.com>
2025-09-10 08:37:25 +0530

    code_changes

--- RestAPI-Documentation/springboot-blog-rest-api-springsecurity-section23/src/main/java/com/test/blog/controller/PostController.java
+++ RestAPI-Documentation/springboot-blog-rest-api-springsecurity-section23/src/main/java/com/test/blog/controller/PostController.java
@@ -4,6 +4,10 @@ import com.test.blog.dtos.PostDTO;
 import com.test.blog.dtos.PostResponse;
 import com.test.blog.service.PostService;
 import com.test.blog.utils.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +18,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
+@Tag(
+        name = "CRUD REST APIs for Post Resource",
+        description = "Create Post, Update Post, Get All Posts, Get Post By Id, Delete Post"
+)
 public class PostController {
 
     private PostService service;
@@ -24,6 +32,13 @@ public class PostController {
 
     //http://localhost:8088/api/posts
     //create blog post Rest API
+    @Operation(
+            summary = "Create Post REST API",
+            description = "Create Post REST API is used to save post into database"
+    )
+    @ApiResponse(responseCode = "201",
+            description = "HTTP Status 201 CREATED")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDTO dto) {
@@ -35,6 +50,12 @@ public class PostController {
     //http://localhost:8088/api/posts?pageNo=0&pageSize=4
     //http://localhost:8088/api/posts?pageNo=0&pageSize=4&sortBy=id
     //http://localhost:8088/api/posts?pageNo=0&pageSize=4&sortBy=id&sortDir=desc
+    @Operation(
+            summary = "Get All Posts REST API",
+            description = "Get All Post REST API is used to Fetch all the posts from the database"
+    )
+    @ApiResponse(responseCode = "200",
+            description = "HTTP Status 200 SUCCESS")
     @GetMapping
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
@@ -45,12 +66,25 @@ public class PostController {
     }
 
     //http://localhost:8088/api/posts/1
+    @Operation(
+            summary = "Get Post REST API by id",
+            description = "Get Post by ID REST API is used to get single post from the database"
+    )
+    @ApiResponse(responseCode = "200",
+            description = "HTTP Status 200 SUCCESS")
     @GetMapping("/{id}")
     public ResponseEntity<PostDTO> getPostById(@PathVariable Long id) {
         return ResponseEntity.ok(service.getPostById(id));
     }
 
     //http://localhost:8088/api/posts/7
+    @Operation(
+            summary = "update Post REST API",
+            description = "Update Post REST API is used to update particular in the database"
+    )
+    @ApiResponse(responseCode = "200",
+            description = "HTTP Status 200 SUCCESS")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDTO> updatePost(@PathVariable("id") Long id, @Valid @RequestBody PostDTO post) {
@@ -58,6 +92,13 @@ public class PostController {
     }
 
     //http://localhost:8088/api/posts/7
+    @Operation(
+            summary = "Delete Post REST",
+            description = "Delete Post REST API is used to delete particular post from the database"
+    )
+    @ApiResponse(responseCode = "200",
+            description = "HTTP Status 200 SUCCESS")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable("id") Long id) {
---
+++ RestAPI-Documentation/springboot-blog-rest-api-springsecurity-section23/src/main/java/com/test/blog/repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.test.blog.repository;
+
+import com.test.blog.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+    @Query()
+    List<Comment> findByPostId(Long postId);
+}
